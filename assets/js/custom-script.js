c9f917229d19507ebfc4dd7902de39494afa1e32
celestin <41481372+c3lestin@users.noreply.github.com>
2023-01-10 19:13:59 +0100

    Create custom-script.js

---
+++ assets/js/custom-script.js
@@ -0,0 +1,31 @@
+// Marckenley Celestin / Electric Zone 2022
+
+const scrollTopWrapper = document.querySelector('.scroll-top-wrapper');
+let isScrolling;
+
+scrollTopWrapper.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+});
+
+
+function checkScrollTop() {
+    if (window.scrollY === 0) {
+        scrollTopWrapper.classList.add('hidden');
+    } else {
+        scrollTopWrapper.classList.remove('hidden');
+    }
+    isScrolling = false;
+}
+
+
+window.addEventListener('scroll', () => {
+    if (isScrolling) return;
+    isScrolling = true;
+    window.requestAnimationFrame(checkScrollTop);
+    
+});
+Footer
